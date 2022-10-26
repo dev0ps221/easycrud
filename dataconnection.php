c23d7c0986011d96cc96069a23129bde883aa84f
dev0ps221 <seybaxscorpion@gmail.com>
2022-10-26 00:19:22 +0000

    konnektdata

---
+++ dataconnection.php
@@ -0,0 +1,21 @@
+<?php
+class KonnektKonnektion{
+    
+    private $connection = null;
+
+    function gotconnection(){
+        return $this->connection != null;
+    }
+
+    function getconnection(){
+        
+    }
+
+    function __connect(){
+
+    }
+
+    function __construct(){
+
+    }
+}
