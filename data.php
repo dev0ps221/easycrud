714d4f4eb66d00e93325f0ef1c1f6115534255a4
dev0ps221 <seybaxscorpion@gmail.com>
2022-10-26 09:13:27 +0000

    deleted:    data.php

--- data.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include('crudconnection.php');
-    $dataconnection = new CrudConnection();
-    if($dataconnection->__connect()){
-        print_r($dataconnection->getdatabasetables()); 
-    }
