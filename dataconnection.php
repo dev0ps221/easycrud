d9ac5729aebec240160348935622f543cbf3a6e4
dev0ps221 <seybaxscorpion@gmail.com>
2022-10-26 00:23:18 +0000

    konnektdata

--- dataconnection.php
+++ dataconnection.php
@@ -2,20 +2,34 @@
 class KonnektKonnektion{
     
     private $connection = null;
+    private $dbuser = null;
+    private $dbpass = null;
+    private $dbhost = null;
+    private $dbname = null;
 
-    function gotconnection(){
+
+    function gotconnection()    {
         return $this->connection != null;
     }
 
-    function getconnection(){
-        
+    function getconnection()    {
+        return $this->connection;
     }
 
-    function __connect(){
-
+    function __connect()    {
+        try {
+            $this->connection = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname",$this->dbuser,$this->dbpass);
+        } catch (\Exception $e)    {
+            error_log(json_encode($e));
+            //throw $th;
+        }
+        return $this->gotconnection();
     }
 
-    function __construct(){
-
+    function __construct($dbhost,$dbuser,$dbpass,$dbname){
+        $this->dbhost = $dbhost;
+        $this->dbuser = $dbuser;
+        $this->dbpass = $dbpass;
+        $this->dbname = $dbname;
     }
 }
