d89dba1311b945d17841c0705b183e9d288258ec
dev0ps221 <seybaxscorpion@gmail.com>
2022-10-26 00:55:42 +0000

    query

--- dataconnection.php
+++ dataconnection.php
@@ -32,15 +32,45 @@ class KonnektKonnektion{
             $this->tables[$key] = [];
             $this->tables[$key]['name'] = $table[0];
             $this->tables[$key]['fields'] = $this->learntablefields($table[0]);
+            $this->generate_table_queries($this->tables[$key]);
         }
         echo "<pre>";
             print_r($this->tables);
         echo "</pre>";
     }
 
+    function generate_field_queries($tablename,$field){
+        $tail = "$tablename_$field";
+        generate_update_function($tablename,$field,$tail);
+    }
+
+    function generate_update_function($tablename,$field,$tail){
+        $fname = "update_$tail";
+        $this->{$fname} = function($value){
+            $fieldname = $field['Field'];
+            $req = "UPDATE $tablename set $fieldname = ";
+            if($field['Type']=='text' or $field['Type']=='char'){
+                $req = "$req '";
+            }
+            $req=$value;
+            if($field['Type']=='text' or $field['Type']=='char'){
+                $req = "$req'";
+            }
+        };        
+    }
+
     function learntablefields($tablename){
         $fields=[];
-
+        $fields['raw'] = [];
+        $fields['usable'] = [];
+        foreach($this->query("describe ".$tablename) as $key=>$field){
+            $fields[$key] = [];
+            array_push($fields['raw'],$field);
+            if($field['Extra']!='auto_increment'){
+                $field = $this->generate_field_queries($tablename,$field);
+                array_push($fields['usable'],$field);
+            }
+        }
         return $fields;
     }
 
